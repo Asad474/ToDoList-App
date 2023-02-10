99e055a99f1f97a0062956a33baf00aa18cfa4a6
Asad Sarfaraz <asadsarfraz474@gmail.com>
2023-02-10 17:24:14 +0530

    ToDoList App(Deployment)

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g%#df=bt_hln4oz8hazwm)wkjh15edbl@72hlbu*zue%3v1a$6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
